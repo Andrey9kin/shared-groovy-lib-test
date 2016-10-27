32c36e659b4d6f22760e53f94775c03c4a0c3f54
Andrey Devyatkin <ady@praqma.net>
2016-10-27 16:14:57 +0200

    Update Bar.groovy

--- src/org/foo/Bar.groovy
+++ src/org/foo/Bar.groovy
@@ -2,7 +2,7 @@ package org.foo
 
 class Bar {
     static def test() {
-        println("test test test!")
+        System.out.println("test test test!")
         return "Hello from the function test!"
    } 
 }
