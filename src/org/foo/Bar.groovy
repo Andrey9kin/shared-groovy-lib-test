3796f2a3bddfcbabee2dc0e7135b39c40b1508c5
Andrey Devyatkin <ady@praqma.net>
2016-11-30 10:47:12 +0100

    Update Bar.groovy

--- src/org/foo/Bar.groovy
+++ src/org/foo/Bar.groovy
@@ -1,8 +1,8 @@
 package org.foo
 
-class Bar {
-    static def test() {
-        echo "test test test!"
-        return "Hello from the function test!"
-   } 
+// vars/helloWorld.groovy
+def call(name) {
+    // you can call any valid step functions from your code, just like you can from Pipeline scripts
+    echo "Hello world, ${name}"
 }
+
